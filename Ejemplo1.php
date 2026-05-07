84d1b38dfcfca9a211ebd8b7984a529db01d91b1
Ricardo <andresbm35@gmail.com>
2026-05-07 16:27:06 -0500

    Cambio titulo pagina

--- Ejemplo1.php
+++ Ejemplo1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario Suma</title>
+    <title>Formulario Sumas</title>
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
